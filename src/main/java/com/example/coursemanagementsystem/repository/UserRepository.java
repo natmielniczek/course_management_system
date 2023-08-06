a86259132b4f07bd5f3dcea5a9fec9a917d2b9fe
Natalia Mielniczek <nat.mielniczek@gmail.com>
2023-08-06 12:49:45 +0200

    fixed spelling mistake in UserRepository

--- src/main/java/com/example/coursemanagementsystem/repository/UserRepository.java
+++ src/main/java/com/example/coursemanagementsystem/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.coursemanagementsystem.repository;
 
+import com.example.coursemanagementsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserRepository, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
